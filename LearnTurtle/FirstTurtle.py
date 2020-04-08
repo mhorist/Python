86f37885d0b4305ba5bcf2778a317cc2dbc5dffa
Matthew Horist <31255620+mhorist@users.noreply.github.com>
2020-04-08 16:29:38 -0400

    added Turtle dir

---
+++ LearnTurtle/FirstTurtle.py
@@ -0,0 +1,21 @@
+import turtle
+
+
+# turtle square
+
+x = turtle.Turtle()
+
+def square():
+    x = turtle.Turtle()
+    x.forward(100)
+    x.right(90)
+    x.forward(100)
+    x.right(90)
+    x.forward(100)
+    x.right(90)
+    x.forward(100)
+
+square()
+
+x.forward(200)
+square()
